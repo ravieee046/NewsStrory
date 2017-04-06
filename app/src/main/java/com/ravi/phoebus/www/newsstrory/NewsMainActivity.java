544f47bf3e1050b9d7323a9248bd9af2dda46534
Ravi Gupta <ravik.rkg@gmail.com>
2017-04-06 15:56:27 +0530

    Initial commit

---
+++ app/src/main/java/com/ravi/phoebus/www/newsstrory/NewsMainActivity.java
@@ -0,0 +1,13 @@
+package com.ravi.phoebus.www.newsstrory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_main);
+    }
+}
