3b69ea1ee44cdd56654f7b87fbdc8c5086c1d1e3
Ravi Gupta <ravik.rkg@gmail.com>
2017-04-07 14:57:56 +0530

    Main Activity and its layout file name change

--- app/src/main/java/com/ravi/phoebus/www/newsstrory/NewsStoryMainActivity.java
+++ app/src/main/java/com/ravi/phoebus/www/newsstrory/NewsStoryMainActivity.java
@@ -3,11 +3,11 @@ package com.ravi.phoebus.www.newsstrory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NewsMainActivity extends AppCompatActivity {
+public class NewsStoryMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news_main);
+        setContentView(R.layout.activity_news_story_main);
     }
 }
